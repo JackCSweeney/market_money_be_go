0e619c0d1ddf6175bff720ae0fc103891d2df2c3
Jack Sweeney <jackcsweeney@gmail.com>
2025-02-04 15:51:26 -0800

    fix: Fix spelling error in get one vendor endpoint func

--- vendors/get_one_vendor.go
+++ vendors/get_one_vendor.go
@@ -8,15 +8,15 @@ import (
 )
 
 func GetOneVendor(c *gin.Context, id int) {
-	sqlStatment := `SELECT * FROM vendors WHERE id=$1`
+	sqlStatement := `SELECT * FROM vendors WHERE id=$1`
 	var vendor Vendor
 
-	row := database.Db.QueryRow(sqlStatment, id)
+	row := database.Db.QueryRow(sqlStatement, id)
 	err := row.Scan(&vendor.ID, &vendor.Name, &vendor.Description, &vendor.ContactName, &vendor.ContactPhone, &vendor.CreditAccepted)
 
 	switch err {
 	case sql.ErrNoRows:
-		c.JSON(http.StatusNotFound, "Error: Record not found")
+		c.JSON(http.StatusNotFound, "Error: Vendor record not found")
 	case nil:
 		c.JSON(http.StatusOK, vendor)
 	default:
