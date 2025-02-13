50dbcf3e15e7fd06f3f58b66a08492aecde82580
Jack Sweeney <jackcsweeney@gmail.com>
2025-02-13 09:28:59 -0800

    feat: Add GetAllMarkets function to return array of Market types

--- models/market.go
+++ models/market.go
@@ -2,7 +2,7 @@ package models
 
 import (
 	"example.com/mod/database"
-	// "fmt"
+	"fmt"
 )
 
 type Market struct {
@@ -24,11 +24,34 @@ func GetMarketById(id int) (Market, error) {
 	err := database.Db.QueryRow(sqlStatement, id).Scan(&market.Id, &market.Name, &market.Street, &market.City, &market.County, &market.State, &market.Zip, &market.Lat, &market.Lon)
 
 	if err != nil {
+		fmt.Println(err)
 		return Market{}, err
 	}
 	return market, nil
 }
 
-// func GetAllMarkets() ([]Market, error) {
+func GetAllMarkets() ([]Market, error) {
+	sqlStatement := `SELECT * FROM markets`
+	var markets []Market
 
-// }
+	rows, err := database.Db.Query(sqlStatement)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var market Market
+		err = rows.Scan(&market.Id, &market.Name, &market.Street, &market.City, &market.County, &market.State, &market.Zip, &market.Lat, &market.Lon)
+		if err != nil {
+			fmt.Printf("Error: Record missing needed information: %s", err)
+		}
+		markets = append(markets, market)
+	}
+	err = rows.Err()
+	if err != nil {
+		return markets, err
+	}
+	return markets, nil
+}
