3a2d3049b239ae646a7c5888a1196919c228a171
Jack Sweeney <jackcsweeney@gmail.com>
2025-02-17 16:23:20 -0800

    feat: Add router.go which handles starting the application server, connecting to the database, and calling the correct handlers based on the routes and context of a request

--- router/router.go
+++ router/router.go
@@ -2,9 +2,8 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"example.com/mod/controllers"
 	"example.com/mod/database"
+	"example.com/mod/handlers"
 	"fmt"
 	"strconv"
 )
@@ -14,7 +13,47 @@ func setupRouter() *gin.Engine {
 	return router
 }
 
-func routes() {
+func Router() {
 	router := setupRouter()
-	database.ConnectDatabase("DB_NAME")
+	database.ConnectDatabase("DB_NAME", ".env")
+
+	publicRoutes := router.Group("/api")
+	// Markets
+	publicRoutes.GET("/markets", handlers.HandleGetAllMarkets)
+	publicRoutes.GET("/markets/:id", func(c *gin.Context) {
+		id, _ := strconv.Atoi(c.Param("id"))
+		handlers.HandleGetMarketById(c, id)
+	})
+	// // Vendors
+	// publicRoutes.GET("/vendors", HandleGetAllVendors())
+	// publicRoutes.GET("/vendors/:id", func(c *gin.Context) {
+	// 	id, _ := strconv.Atoi(c.Param("id"))
+	// 	HandleGetVendorById()
+	// })
+	// publicRoutes.POST("/vendors", HandleCreateVendor())
+	// publicRoutes.PATCH("/vendors/:id", func(c *gin.Context) {
+	// 	id, _ := strconv.Atoi(c.Param("id"))
+	// 	HandleUpdateVendor()
+	// })
+	// // Market Vendors
+	// publicRoutes.GET("/markets/:id/vendors", func(c *gin.Context) {
+	// 	id, _ := strconv.Atoi(c.Param("id"))
+	// 	HandleGetAllVendorsForMarket()
+	// })
+	// publicRoutes.POST("/markets/:id/vendors", func(c *gin.Context) {
+	// 	id, _ := strconv.Atoi(c.Param("id"))
+	// 	HandleCreateMarketVendor()
+	// })
+	// publicRoutes.DELETE("/markets/:id/vendors", func(c *gin.Context) {
+	// 	id, _ := strconv.Atoi(c.Param("id"))
+	// 	HandleDeleteMarketVendor()
+	// })
+
+	err := router.Run(":8080")
+	if err != nil {
+		fmt.Println("Error starting server on port :8080")
+		panic(err)
+	}
+
+	fmt.Println("Now listening and serving on port :8080")
 }
